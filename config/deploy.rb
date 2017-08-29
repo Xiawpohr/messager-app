5ae833d97f8a6019aa597c457719243a8890910d
XiawPohr <paradise121329@gmail.com>
2017-08-29 14:17:39 +0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:Xiawpohr/messager-app.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/messager-app"
+set :deploy_to, "/home/messager/messager-app"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
